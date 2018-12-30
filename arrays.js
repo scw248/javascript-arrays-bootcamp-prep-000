d67d6718b56596fe171c21bd283507b737a741f5
Stephen Wright <scw248@gmail.com>
2018-12-30 23:52:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,22 @@ function accessElementInArray(array, index){
   return array[index];
 }
 
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift(array[0]);
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop();
+}
+
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, -1);
+}
+
+  
+
